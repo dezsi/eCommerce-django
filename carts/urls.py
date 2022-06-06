aee67550b3d7174401b8f62a85a5d764046f3d45
dezsi <dezsi.janos@yahoo.com>
2022-06-06 19:04:10 +0300

    initial commit

--- carts/urls.py
+++ carts/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path('remove_cart/<int:product_id>/', views.remove_cart, name='remove_cart'),
     path('remove_cart_item/<int:product_id>/', views.remove_cart_item, name='remove_cart_item'),
 
+    path('checkout/', views.checkout, name='checkout'),
+    path('order/', views.order, name='order'),
+
 
 ]
--- carts/views.py
+++ carts/views.py
@@ -79,3 +79,32 @@ def cart(request, total=0,  quantity=0, cart_items=None):
         'grand_total': grand_total,
     }
     return render(request, 'store/cart.html', context)
+
+
+
+def checkout(request, total=0,  quantity=0, cart_items=None):
+    try:
+        TVA=0
+        grand_total = 0
+        cart=Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_Active=True)
+        for cart_item in cart_items:
+            total += (cart_item.product.price * cart_item.quantity)
+            quantity += cart_item.quantity
+        TVA = (19 * total)/100
+        grand_total = total + TVA
+    except ObjectDoesNotExist:
+        pass
+
+    context={
+        'total': total,
+        'quantity': quantity,
+        'cart_items': cart_items,
+        'TVA': TVA,
+        'grand_total': grand_total,
+    }
+    return render(request, "store/checkout.html", context)
+
+
+def order(request):
+    return render(request, 'store/cart/order.html')
